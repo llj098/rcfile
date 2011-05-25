b2053c88421bb9ce6d44b099cc7ef2ae16cf25af
llj098 <llj098@gmail.com>
2011-05-25 22:48:13 +0800

    commit

--- script/syncrc.sh
+++ script/syncrc.sh
@@ -1,7 +1,7 @@
-sudo cp -r .vim* ~/rc/
-sudo cp -r .zsh* ~/rc/
-sudo cp -r .bash* ~/rc/
-sudo cp -r .git* ~/rc/
+sudo cp -r ~/.vim* ~/rc/
+sudo cp -r ~/.zsh* ~/rc/
+sudo cp -r ~/.bash* ~/rc/
+sudo cp -r ~/.git* ~/rc/
 sudo cp -r ~/script/ ~/rc/
 cd ~/rc/
 git add .
